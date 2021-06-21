883dd874b173483bbf01c962dcafc961333c046a
restrictedCodex <restrictedcharudutta@gmail.com>
2021-06-21 06:40:52 +0530

    bug fixed

--- src/Components/Trips/Tripcount.js
+++ src/Components/Trips/Tripcount.js
@@ -13,10 +13,10 @@ export const Tripcount = ({total,tropic,trek,club, goal=15}) => {
     return (
         <div className='trips-count'>
             <div className='content'>
-                <div className='total'><h1>Total {total}</h1>trips</div>
-                <div className='trek'><h1><i class="fas fa-campground"></i>{trek}</h1>trips</div>
-                <div className='club'><h1><i class="fas fa-coffee"></i>{club}</h1>trips</div>
-                <div className='tropic'><h1><i class="fas fa-umbrella-beach"></i>{tropic}</h1>trips</div>
+                <div className='total'><h1>Total {total}</h1>Trips</div>
+                <div className='trek'><h1><i class="fas fa-campground"></i>{trek}</h1>Trips</div>
+                <div className='club'><h1><i class="fas fa-coffee"></i>{club}</h1>Trips</div>
+                <div className='tropic'><h1><i class="fas fa-umbrella-beach"></i>{tropic}</h1>Trips</div>
                 <div className='goal'>
                     <div className='progress' style={getProgressBarWidth(getProgress(total,goal))}>   
                     </div>
